31d196658c19da43b9224be2f874cbeff875c912
Pam Mantri <pmantri@mskilab02.c.nygenome.org>
2025-04-07 16:11:58 -0400

    adding more edfs

--- R/figs_extd.R
+++ R/figs_extd.R
@@ -18,6 +18,10 @@ library(ggpubr)
 library(ggforce)
 
 
+
+
+
+
 # ------------------------------------------------------------------------------------------------
 # EDF 6A
 # ------------------------------------------------------------------------------------------------
@@ -63,6 +67,11 @@ print(p_acc)
 
 
 
+
+
+
+
+
 # ------------------------------------------------------------------------------------------------
 # EDF 8A
 # ------------------------------------------------------------------------------------------------
@@ -93,5 +102,175 @@ Heatmap(t(mat.in.2[,1:23]), name = "Relative Risk", col = col_fun2, cluster_rows
 
 
 
+
+# ------------------------------------------------------------------------------------------------
+# EDF 9C
+# ------------------------------------------------------------------------------------------------
+
+edf9 <- readRDS('../data/edf9.rds')
+
+ik <-  'Papillary'
+
+
+freq.plot.tmp.f = edf9
+
+freq.plot.tmp.f$clust.int = freq.plot.tmp.f$T_Identity
+freq.plot.tmp.f = freq.plot.tmp.f[!duplicated(freq.plot.tmp.f),]
+col.num = which(colnames(freq.plot.tmp.f) == ik)
+freq.plot.tmp.f$mut.int = freq.plot.tmp.f[,..col.num]
+freq.plot.tmp.f = freq.plot.tmp.f[!is.na(mut.int),]
+freq.plot.tmp.f$mut.int = as.numeric(freq.plot.tmp.f$mut.int)
+res.plot = freq.plot.tmp.f[, prop.test(sum(mut.int), .N) %>% dflm %>% cbind(nmut = sum(mut.int), tot = .N), by = .(clust.int)][, fracmut := estimate]
+res.plot$clust.int = factor(res.plot$clust.int, levels = c('Non-distal identity','Distal identity'))
+non.dist.mut = res.plot[clust.int == 'Non-distal identity',]$nmut
+non.dist.wt = res.plot[clust.int == 'Non-distal identity',]$tot - res.plot[clust.int == 'Non-distal identity',]$nmut
+dist.mut = res.plot[clust.int == 'Distal identity',]$nmut
+dist.wt = res.plot[clust.int == 'Distal identity',]$tot - res.plot[clust.int == 'Distal identity',]$nmut
+
+prox.dist.fisher = matrix(c(non.dist.mut, dist.mut, non.dist.wt, dist.wt), nrow = 2, byrow = TRUE) %>% fisher.test %>% dflm
+
+
+ggplot(res.plot, aes(x = clust.int, y = fracmut, fill = clust.int)) +
+  geom_bar(stat = 'identity', position = position_dodge()) +
+  geom_errorbar(aes(ymin = ci.lower, ymax = ci.upper), width = 0.15) + theme_bw() + 
+  scale_fill_manual(values = c(  'forestgreen', 'darkgoldenrod3')) +
+  labs(title = paste0('Papillary fraction of LUAD identity'), x = '', y = '') + theme_classic() +
+  theme(plot.title = element_text(size = 20, face = 'bold'),
+        axis.text.x = element_text(size = 0, angle = 0, hjust = 0.5),
+        axis.text.y = element_text(size = 22, angle = 0, hjust = 1),
+        axis.title.x = element_text(size = 0, face = 'plain'),
+        axis.title.y = element_text(size = 5, face = 'bold'),
+        axis.ticks.x = element_blank()) + 
+  geom_text(mapping = aes(x = clust.int, y = ci.upper + 0.05, label = paste0(nmut, '/', tot)), size = 7) +
+  guides(fill = guide_legend(title = 'Identity')) + theme(legend.position = "top")
+
+
+
+
+
+
+# ------------------------------------------------------------------------------------------------
+# EDF 9D
+# ------------------------------------------------------------------------------------------------
+# by Identity 
+
+edf9 <- readRDS('../data/edf9.rds')
+
+ik <-  'NSCLC_NOS'
+
+freq.plot.tmp.f = edf9
+
+freq.plot.tmp.f$clust.int = freq.plot.tmp.f$T_Identity
+freq.plot.tmp.f = freq.plot.tmp.f[!duplicated(freq.plot.tmp.f),]
+col.num = which(colnames(freq.plot.tmp.f) == ik)
+freq.plot.tmp.f$mut.int = freq.plot.tmp.f[,..col.num]
+freq.plot.tmp.f = freq.plot.tmp.f[!is.na(mut.int),]
+freq.plot.tmp.f$mut.int = as.numeric(freq.plot.tmp.f$mut.int)
+res.plot = freq.plot.tmp.f[, prop.test(sum(mut.int), .N) %>% dflm %>% cbind(nmut = sum(mut.int), tot = .N), by = .(clust.int)][, fracmut := estimate]
+res.plot$clust.int = factor(res.plot$clust.int, levels = c('Non-distal identity','Distal identity'))
+non.dist.mut = res.plot[clust.int == 'Non-distal identity',]$nmut
+non.dist.wt = res.plot[clust.int == 'Non-distal identity',]$tot - res.plot[clust.int == 'Non-distal identity',]$nmut
+dist.mut = res.plot[clust.int == 'Distal identity',]$nmut
+dist.wt = res.plot[clust.int == 'Distal identity',]$tot - res.plot[clust.int == 'Distal identity',]$nmut
+
+prox.dist.fisher = matrix(c(non.dist.mut, dist.mut, non.dist.wt, dist.wt), nrow = 2, byrow = TRUE) %>% fisher.test %>% dflm
+
+
+ggplot(res.plot, aes(x = clust.int, y = fracmut, fill = clust.int)) +
+  geom_bar(stat = 'identity', position = position_dodge()) +
+  geom_errorbar(aes(ymin = ci.lower, ymax = ci.upper), width = 0.15) + theme_bw() + 
+  scale_fill_manual(values = c(  'forestgreen', 'darkgoldenrod3', '#bdbdbd')) +
+  labs(title = paste0('NSCLC-NOS fraction of LUAD identity'), x = '', y = '') + theme_classic() +
+  theme(plot.title = element_text(size = 20, face = 'bold'),
+        axis.text.x = element_text(size = 0, angle = 0, hjust = 0.5),
+        axis.text.y = element_text(size = 22, angle = 0, hjust = 1),
+        axis.title.x = element_text(size = 0, face = 'plain'),
+        axis.title.y = element_text(size = 5, face = 'bold'),
+        axis.ticks.x = element_blank()) + 
+  geom_text(mapping = aes(x = clust.int, y = ci.upper + 0.05, label = paste0(nmut, '/', tot)), size = 7) +
+  guides(fill = guide_legend(title = 'Identity')) + theme(legend.position = "top")
+
+
+
+
+
+# ------------------------------------------------------------------------------------------------
+# EDF 9E
+# ------------------------------------------------------------------------------------------------
+# NSCLC-NOS vs TP53
+
+edf9 <- readRDS('../data/edf9.rds')
+
+ik <-  'NSCLC_NOS'
+
+freq.plot.tmp.f = edf9
+
+freq.plot.tmp.f$clust.int = freq.plot.tmp.f$TP53_mut
+freq.plot.tmp.f = freq.plot.tmp.f[!duplicated(freq.plot.tmp.f),]
+col.num = which(colnames(freq.plot.tmp.f) == ik)
+freq.plot.tmp.f$mut.int = freq.plot.tmp.f[,..col.num]
+freq.plot.tmp.f = freq.plot.tmp.f[!is.na(mut.int),]
+freq.plot.tmp.f$mut.int = as.numeric(freq.plot.tmp.f$mut.int)
+res.plot = freq.plot.tmp.f[, prop.test(sum(mut.int), .N) %>% dflm %>% cbind(nmut = sum(mut.int), tot = .N), by = .(clust.int)][, fracmut := estimate]
+res.plot$clust.int = factor(res.plot$clust.int, levels = c('TP53 MUT','WT'))
+non.dist.mut = res.plot[clust.int == 'TP53 MUT',]$nmut
+non.dist.wt = res.plot[clust.int == 'TP53 MUT',]$tot - res.plot[clust.int == 'TP53 MUT',]$nmut
+dist.mut = res.plot[clust.int == 'WT',]$nmut
+dist.wt = res.plot[clust.int == 'WT',]$tot - res.plot[clust.int == 'WT',]$nmut
+
+prox.dist.fisher = matrix(c(non.dist.mut, dist.mut, non.dist.wt, dist.wt), nrow = 2, byrow = TRUE) %>% fisher.test %>% dflm
+
+
+ggplot(res.plot, aes(x = clust.int, y = fracmut, fill = clust.int)) +
+  geom_bar(stat = 'identity', position = position_dodge()) +
+  geom_errorbar(aes(ymin = ci.lower, ymax = ci.upper), width = 0.15) + theme_bw() + 
+  scale_fill_manual(values = c(  'salmon3', 'snow4')) +
+  labs(title = paste0('NSCLC-NOS histology fraction'), x = '', y = '') + theme_classic() +
+  theme(plot.title = element_text(size = 20, face = 'bold'),
+        axis.text.x = element_text(size = 0, angle = 0, hjust = 0.5),
+        axis.text.y = element_text(size = 22, angle = 0, hjust = 1),
+        axis.title.x = element_text(size = 0, face = 'plain'),
+        axis.title.y = element_text(size = 5, face = 'bold'),
+        axis.ticks.x = element_blank()) + 
+  geom_text(mapping = aes(x = clust.int, y = ci.upper + 0.05, label = paste0(nmut, '/', tot)), size = 7) +
+  guides(fill = guide_legend(title = 'TP53 status')) + theme(legend.position = "top")
+
+# ------------------------------------------------------------------------------------------------
+# EDF 9F
+# ------------------------------------------------------------------------------------------------
+
+edf9 <- readRDS('../data/edf9.rds')
+
+ik = 'mut_tp53_hist'
+freq.plot.tmp.f = edf9
+
+freq.plot.tmp.f$clust.int = freq.plot.tmp.f$NSCLC_NOS_VS_Others
+freq.plot.tmp.f = freq.plot.tmp.f[!duplicated(freq.plot.tmp.f),]
+col.num = which(colnames(freq.plot.tmp.f) == ik)
+freq.plot.tmp.f$mut.int = freq.plot.tmp.f[,..col.num]
+freq.plot.tmp.f = freq.plot.tmp.f[!is.na(mut.int),]
+freq.plot.tmp.f$mut.int = as.numeric(freq.plot.tmp.f$mut.int)
+res.plot = freq.plot.tmp.f[, prop.test(sum(mut.int), .N) %>% dflm %>% cbind(nmut = sum(mut.int), tot = .N), by = .(clust.int)][, fracmut := estimate]
+res.plot$clust.int = factor(res.plot$clust.int, levels = c('NSCLC_NOS','Non-NSCLC_NOS'))
+nsclc.nos.mut = res.plot[clust.int == 'NSCLC_NOS',]$nmut
+nsclc.nos.wt = res.plot[clust.int == 'NSCLC_NOS',]$tot - res.plot[clust.int == 'NSCLC_NOS',]$nmut
+others.mut = res.plot[clust.int == 'Non-NSCLC_NOS',]$nmut
+others.wt = res.plot[clust.int == 'Non-NSCLC_NOS',]$tot - res.plot[clust.int == 'Non-NSCLC_NOS',]$nmut
+
+nsclc.nos.others.fisher = matrix(c(nsclc.nos.mut, others.mut, nsclc.nos.wt, others.wt), nrow = 2, byrow = TRUE) %>% fisher.test %>% dflm
+
+ggplot(res.plot, aes(x = clust.int, y = fracmut, fill = clust.int)) +
+  geom_bar(stat = 'identity', position = position_dodge()) +
+  geom_errorbar(aes(ymin = ci.lower, ymax = ci.upper), width = 0.15) + theme_bw() + 
+  scale_fill_manual(values = c(  'orchid3',   'snow4')) +
+  labs(title = paste0('Fraction of samples with TP53 mut'), x = '', y = '') + theme_classic() +
+  theme(plot.title = element_text(size = 20, face = 'bold'),
+        axis.text.x = element_text(size = 0, angle = 0, hjust = 0.5),
+        axis.text.y = element_text(size = 22, angle = 0, hjust = 1),
+        axis.title.x = element_text(size = 0, face = 'plain'),
+        axis.title.y = element_text(size = 5, face = 'bold'),
+        axis.ticks.x = element_blank()) + 
+  geom_text(mapping = aes(x = clust.int, y = ci.upper + 0.05, label = paste0(nmut, '/', tot)), size = 7) + ylim(0,1.05) +
+  guides(fill = guide_legend(title = 'Histology')) + theme(legend.position = "top")
 
 
